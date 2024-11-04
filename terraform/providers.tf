6ef3c107fc50c82dd01984396880b87a578fc40e
Scott Burgholzer <scott@scottburgholzer.tech>
2024-11-04 04:07:35 +0000

    testing tflint PR comment

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -31,32 +31,37 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.69.0"
     }
-    # http = {
-    #   source = "hashicorp/http"
-    #   version = "3.4.5"
-    # }
   }
 }
 
-provider "aws" {
-  region = var.region
-  default_tags {
-    tags = {
-      environment = "Production" # lowercase will cause an issue
-      Owner       = "Scott"
-      ManagedBy   = "Terraform"
-      Project     = "GitOps MiniCamp"
-      unused_tag  = "this should cause an issue" # extra tag
-    }
-  }
+# Unused variable
+variable "unused_var" {
+  type = string
+  default = "this will cause an error"
 }
 
-# Adding a resource without proper tags
-resource "aws_s3_bucket" "test" {
-  bucket = "my-test-bucket"
-  
-  tags = {
-    # Missing required tags will cause issues
-    random_tag = "value"
-  }
+# Duplicate resource names
+resource "aws_s3_bucket" "example" {
+  bucket = "my-bucket-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-bucket-2"
+}
+
+# Invalid interpolation syntax
+resource "aws_instance" "bad_syntax" {
+  ami           = "${var.ami_id"  # Missing closing brace
+  instance_type = "${var.instance_type}}}"  # Extra closing braces
+}
+
+# Reference to undeclared variable
+resource "aws_vpc" "main" {
+  cidr_block = var.undefined_variable
+}
+
+# Unnecessary interpolation
+resource "aws_subnet" "example" {
+  vpc_id     = "${aws_vpc.main.id}"  # Unnecessary interpolation
+  cidr_block = "${var.subnet_cidr}"  # Unnecessary interpolation
 }
